b674855fa3759c331f513e819b1dda8cc3447463
dataNerd65 <pkiarie811@gmail.com>
2024-07-27 17:03:13 +0300

    Set up start : for servlet

--- src/main/java/com/example/erehab/LoginServlet.java
+++ src/main/java/com/example/erehab/LoginServlet.java
@@ -6,17 +6,6 @@ import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "loginServlet", value = "/login")
 public class LoginServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<form method='POST' action='login'>");
-        out.println("Username: <input type='text' name='username'><br>");
-        out.println("Password: <input type='password' name='password'><br>");
-        out.println("<input type='submit' value='login'>");
-        out.println("</form>");
-        out.println("</body></html>");
-    }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
