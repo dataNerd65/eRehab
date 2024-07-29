1f22f2ead4580fd0ef546fbf60fa8c68df735e02
dataNerd65 <pkiarie811@gmail.com>
2024-07-29 15:18:17 +0300

    Minor changes, testing ... success

--- src/main/java/com/example/erehab/Main.java
+++ src/main/java/com/example/erehab/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //loading environment variables in main
         Dotenv dotenv = Dotenv.load();
-        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
         System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
         System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
