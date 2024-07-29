ec51ac4b97a7a0dac9ca7773b12420012baf0606
dataNerd65 <pkiarie811@gmail.com>
2024-07-29 11:25:08 +0300

    Solving a 404 error

--- src/main/java/com/example/erehab/LoginServlet.java
+++ src/main/java/com/example/erehab/LoginServlet.java
@@ -3,6 +3,8 @@ package com.example.erehab;
 import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 @WebServlet(name = "loginServlet", value = "/login")
 public class LoginServlet extends HttpServlet {
@@ -10,6 +12,30 @@ public class LoginServlet extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         //Authentication logic
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
+
+        try{
+            transaction = session.beginTransaction();
+            User user = (User) session.createQuery("FROM User U WHERE U.username = :username")
+                    .setParameter("username", username)
+                    .uniqueResult();
+
+            if (user != null && user.getPassword().equals(password)) {
+                response.sendRedirect("welcome.jsp");
+            }else{
+                response.sendRedirect("login.jsp?error=true");
+            }
+
+            transaction.commit();
+        } catch(Exception e){
+            if(transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
         response.sendRedirect("index.jsp");
     }
 
