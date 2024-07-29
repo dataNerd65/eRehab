7364357fb714fcfca2e0aaa905d3c95f9576c712
dataNerd65 <pkiarie811@gmail.com>
2024-07-29 11:40:23 +0300

    Hibernate database testing success

--- src/main/java/com/example/erehab/Main.java
+++ src/main/java/com/example/erehab/Main.java
@@ -1,7 +1,15 @@
 package com.example.erehab;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class Main {
     public static void main(String[] args) {
+        //loading environment variables in main
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
         HibernateUtil.testConnection();
     }
 }
