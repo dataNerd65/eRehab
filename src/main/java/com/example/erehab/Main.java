11e0ec1886c848d7fd866340a34e6b41ac4afa82
dataNerd65 <pkiarie811@gmail.com>
2024-07-29 11:26:29 +0300

    A main function first to test hibernate

--- src/main/java/com/example/erehab/Main.java
+++ src/main/java/com/example/erehab/Main.java
@@ -1,2 +1,7 @@
-package com.example.erehab;public class Main {
+package com.example.erehab;
+
+public class Main {
+    public static void main(String[] args) {
+        HibernateUtil.testConnection();
+    }
 }
